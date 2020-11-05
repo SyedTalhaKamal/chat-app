e2c2379b81bfd409aa54bbe9d7be3754909f5b37
SyedTalhaKamal <syedtalhakamal22@gmail.com>
2020-11-05 10:33:58 +0500

    Chat App with node js and mongo db

---
+++ read.js
@@ -0,0 +1,26 @@
+// ================================read File=========================================================
+// var fs= require('fs');
+// var dataValue= require('./filedata.json')
+// console.log(dataValue.name);
+// fs.readFile('./filedata.json','utf-8', (err,data)=>{
+//     var data= JSON.parse(data)
+//     console.log(data.name);
+// })
+
+
+// ================================Access Directories=========================================================
+// var fs = require('fs');
+// fs.readdir('d:/psd-to-html/',(err,data)=>{
+//     console.log(data);
+// })
+
+// ================================Write Files=========================================================
+// var fs = require('fs')
+// var mydata = {
+//     name: 'Talha-kamal'
+// }
+// fs.writeFile('data.json' , JSON.stringify(mydata), (err)=>{
+//     console.log("write finished", err);
+// })
+
+
